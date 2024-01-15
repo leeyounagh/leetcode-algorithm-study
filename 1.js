cb3e6f7c9fdc122b52acec9461957818ad862029
leesuyoen <dudgk0216@sqkcloud.com>
2024-01-15 16:02:05 +0900

    first commit

---
+++ 1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let i =0; let j =0;
+       let merged =''
+    while(i < word1.length && j <word2.length){
+       merged += word1[i++] + word2[j++]
+    }
+    while (i < word1.length){
+       merged += word1[i++]
+    }
+    while (j < word2.length){
+           merged += word2[j++]
+    }
+    return merged
+};
