43968b0773616448a36c814bdbf254897634f7df
leesuyoen <dudgk0216@sqkcloud.com>
2024-01-18 14:33:56 +0900

    정렬

--- 1431.js
+++ 1431.js
@@ -6,7 +6,7 @@
 var kidsWithCandies = function(candies, extraCandies) {
     
     //임의의 i번째 아이에게 내가가진 사탕을 몰빵해줬을때 i번째 아이가 가장많은 사탕을 가지면 true, 아니면 false
-      let maxCandies = Math.max(...candies); 
+    let maxCandies = Math.max(...candies); 
     let result = candies.map(candy => candy + extraCandies>= maxCandies);
     return result;
 };
