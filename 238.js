1be72f2e4970e5b3d43dac5e11e679ba5770f7c9
[IoT1]이수연 <dudgk0216@gmail.com>
2024-01-21 16:59:35 +0900

    238 완료

---
+++ 238.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  let answerArr = [];
+  let left = 1,
+    right = 1;
+  let n = nums.length;
+  let output = new Array(n).fill(1);
+
+  for (let i = 0; i < n; i++) {
+    output[i] = left;
+    left *= nums[i];
+  }
+
+  for (let j = n - 1; j >= 0; j--) {
+    output[j] *= right;
+    right *= nums[j];
+  }
+
+  return output;
+  //   투포인터 활용
+};
