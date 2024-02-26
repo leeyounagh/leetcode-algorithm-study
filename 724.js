e3589a82b5916e614ae0a13365005ede72673ad7
leesuyoen <dudgk0216@sqkcloud.com>
2024-02-26 16:53:06 +0900

    724

---
+++ 724.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let totalSum = nums.reduce((acc,cur)=> acc+cur,0);
+    let leftSum = 0;
+
+    for(let i =0; i <nums.length; i++){
+        let rightSum = totalSum - leftSum - nums[i];
+
+        if(leftSum ===rightSum){
+            return i
+        }
+        leftSum += nums[i];
+    }
+    return -1
+};
